984eefbf266c9fff934515f7dc5a19c45232cd34
zwhi0003 <zwhi0003@student.monash.edu>
2023-08-09 09:05:56 +1000

    init

---
+++ FIT5032_MyCodeSnippet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FIT5032_MyCodeSnippet.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
