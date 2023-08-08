c7d148a4989814040265a09b853b3312a3c217dc
zwhi0003 <zwhi0003@student.monash.edu>
2023-08-09 09:42:50 +1000

    lab 3

--- FIT5032_MyCodeSnippet/Controllers/HomeController.cs
+++ FIT5032_MyCodeSnippet/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using FIT5032_MyCodeSnippet.Models;
+using FIT5032_MyCodeSnippet.Models.HellowWorld;
 
 namespace FIT5032_MyCodeSnippet.Controllers;
 
@@ -15,6 +16,18 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+        return View();
+    }
+
+    public ActionResult About()
+    {
+        // I commented this line out
+        //ViewBag.Message = "Your application description page.";
+        // I created an instance of the Hello object
+        Hello hello = new Hello();
+        // I assigned the ViewBag.Message to the result
+        ViewBag.Message = hello.GetHello();
+
         return View();
     }
 
---
+++ FIT5032_MyCodeSnippet/Models/HellowWorld/Hello.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace FIT5032_MyCodeSnippet.Models.HellowWorld
+{
+	public class Hello
+	{
+        public String GetHello()
+        {
+            return "Hello World!";
+        }
+    }
+}
+
